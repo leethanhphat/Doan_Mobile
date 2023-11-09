0f69e06d2e090d3bcde04a8fc9abcd122adeb674
Lee Thanh Phat <phat1208gct@gmail.com>
2023-11-10 00:37:18 +0700

    update1

---
+++ Doan_Mobile/app/src/main/java/com/example/tryagain/Activity/End_Process_Activity.java
@@ -0,0 +1,29 @@
+package com.example.tryagain.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tryagain.R;
+
+public class End_Process_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_process);
+        Button btn_return = findViewById(R.id.btn_return);
+
+        btn_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(End_Process_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            }
+        });
+    }
+}
